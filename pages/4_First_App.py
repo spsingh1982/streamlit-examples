1933f47d0012a63213941d13c4874499541b810f
Sunny Singh <sunnypalsingh@gmail.com>
2024-02-17 19:30:54 -0600

    Initial Changes

---
+++ pages/4_First_App.py
@@ -0,0 +1,49 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any
+
+import numpy as np
+
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+
+def first_app() -> None:
+
+    st.header('st.button')
+
+    if st.button('Say hello'):
+       st.write('Why hello there')
+    else:
+       st.write('Goodbye')
+        
+    # Streamlit widgets automatically run the script from top to bottom. Since
+    # this button is not connected to any other logic, it just causes a plain
+    # rerun.
+    st.button("Re-run")
+
+
+st.set_page_config(page_title="First App", page_icon="📹")
+st.markdown("# First App")
+st.sidebar.header("First App")
+st.write(
+    """This app shows how you can use Streamlit to build cool animations.
+It displays an animated fractal based on the the Julia Set. Use the slider
+to tune different parameters."""
+)
+
+first_app()
+
+show_code(animation_demo)
